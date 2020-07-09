5bf7c4ef51d2ea379466f6b1ad71e766aa229b8d
Benoit Jupille <benoit.jupille@capgemini.com>
2020-07-09 07:31:03 +0200

    Refactored the simple blink program to test function creation
    Moved the delay part to a function

--- blink.go
+++ blink.go
@@ -1,21 +1,26 @@
 package main
 
 import (
-  "machine"
-  "time"
+	"machine"
+	"time"
 )
 
 func main() {
-  // chosing pin 13, the built in led on arduino cards
-  var led machine.Pin = machine.Pin(13)
+	// chosing pin 13, the built in led on arduino cards
+	var led machine.Pin = machine.Pin(13)
 
-  // set output mode
-  led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	// set output mode
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-  for {
-    led.High() // led up
-    time.Sleep(time.Millisecond * 500)
-    led.Low() // led off
-    time.Sleep(time.Millisecond * 500)
-  }
+	for {
+		led.High() // led up
+		delay(500) // wait
+		led.Low() // led off
+		delay(500) // wait
+	}
+}
+
+// Put the execution to sleep during the given time in milliseconds
+func delay(milliseconds int64) {
+	time.Sleep(time.Duration(1000000 * milliseconds))
 }
